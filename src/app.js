e62d2c69bced0fbb00055b69236916056b82d3f4
bibhuprasadrout <bibhu.rs@gmail.com>
2025-02-20 23:37:54 +0530

    create a category model and create a get api to use the model

--- src/app.js
+++ src/app.js
@@ -6,14 +6,22 @@ const cors = require("cors");
 // const landingPage = require("./modules/landingPage.js")
 
 const Category = require("./models/category.js")
-const { Model } = require("mongoose")
-app.use(express.json())
+const { Model } = require("mongoose");
+const { skip } = require("node:test");
+app.use(express.json());
 app.use(cors());
 
 app.get("/api/", async (req, res) => {
+  fetchItems = req.query.fetchItems || 0;
+  skipItems = req.query.skipItems || 0;
+  let categories;
   try {
-    const categories = await Category.find();
-    res.send(categories);
+    fetchItems === "0" || fetchItems === 0
+      ? (categories = await Category.find({ _id: null }))
+      : (categories = await Category.find().skip(skipItems).limit(fetchItems));
+    const categoriesLength = await Category.estimatedDocumentCount();
+    const response = { categories, categoriesLength };
+    res.send(response);
   } catch (err) {
     console.log("Something went wrong:", err);
     res.status(500).json({ message: "Internal server error" });
--- src/models/category.js
+++ src/models/category.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose")
 const { Schema } = mongoose
 const chategorySchema = new Schema({
-  slug: String,
-  name: String,
-  url: String,
-  image: String
-},)
+  slug: { type: String, required: true },
+  name: { type: String, required: true },
+  url: { type: String },
+  image: { type: String },
+});
   
 const Category = mongoose.model("Category", chategorySchema)
 
