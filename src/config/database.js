b4834191b1eb1a192fcf2ffec554078397133bfb
bibhuprasadrout <bibhu.rs@gmail.com>
2026-01-04 08:42:36 +0530

    deleteing unwanted files-refactor code-bibhu

--- src/config/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://bibhurs:1234@toycluster.ddrg3.mongodb.net/basic-ecom-database?retryWrites=true&w=majority&appName=ToyCluster")
-}
-
-module.exports = connectDB;
